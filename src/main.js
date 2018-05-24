bb989767031d995611b0a24c9152bf4d43dd2525
Alex Noble and Kristi Hwang <kristi@>
2018-05-24 10:02:38 -0700

    add battle button, add enemy player, add enemy stats

--- src/main.js
+++ src/main.js
@@ -28,18 +28,80 @@ $(document).ready(function() {
     var myProkemon = new Prokemon(name, type);
 
     //var myProkemon = newProkemon(type);
-    $('#status-area').append("<h3>"+myProkemon.name+"</h3>");
-    $('#status-area').append("<h3>"+myProkemon.type+"</h3>");
+    // $('#status-area').append("<h3>"+myProkemon.name+"</h3>");
+    // $('#status-area').append("<h3>"+myProkemon.type+"</h3>");
+    $('#playerNameDisplay').text(myProkemon.name);
+    $('#playerTypeDisplay').text(myProkemon.type);
     $('#playerLevelDisplay').text(myProkemon.level);
     $('#playerHPDisplay').text(myProkemon.hp);
     $('#playerAbilitiesDisplay').text(myProkemon.ability);
 
     $('#status-area').toggle();
+    $('#chooseEnemy').toggle();
 
     // var goal = $('#goal').val();
     // var output = starter(goal);
     // output.forEach(function(element) {
     //   $('#solution').append("<li>" + element + "</li>");
     // });
+    // $("#battle").click(function(event) {
+    //   $("#enemy").show();
+    //
+    //   var name= "enemagnimous";
+    //   var enemy=$('input:radio[name=type]:checked').val();
+    //   var image="";
+    //   var ability="";
+    //
+    //   if (type=="rock"){
+    //     $("#rockImage").toggle();
+    //   } else if (type=="fire"){
+    //     $("#fireImage").toggle();
+    //   } else if (type=="box"){
+    //     $("#boxImage").toggle();
+    //   }
+    //
+    //   var enemyProkemon = new Prokemon(name, type);
+    //
+    // });
+  });
+
+  $('#enemyButton1').click(function() {
+    var enemyProkemon = new Prokemon("Bart", "rock");
+    $('#enemyNameDisplay').text(enemyProkemon.name);
+    $('#enemyTypeDisplay').text(enemyProkemon.type);
+    $('#enemyLevelDisplay').text(enemyProkemon.level);
+    $('#enemyHPDisplay').text(enemyProkemon.hp);
+    $('#enemyAbilitiesDisplay').text(enemyProkemon.ability);
+    $('#chooseEnemy').toggle();
+    $('#enemy').show();
+    $('#enemyRockImage').toggle();
+    //$('enemy-proke-stats').toggle();
+  });
+  $('#enemyButton2').click(function() {
+    var enemyProkemon = new Prokemon("Dan", "box");
+    $('#enemyNameDisplay').text(enemyProkemon.name);
+    $('#enemyTypeDisplay').text(enemyProkemon.type);
+    $('#enemyLevelDisplay').text(enemyProkemon.level);
+    $('#enemyHPDisplay').text(enemyProkemon.hp);
+    $('#enemyAbilitiesDisplay').text(enemyProkemon.ability);
+    $('#chooseEnemy').toggle();
+    $('#enemy').show();
+    $('#enemyBoxImage').toggle();
+    //$('enemy-proke-stats').toggle();
+  });
+  $('#enemyButton3').click(function() {
+    var enemyProkemon = new Prokemon("Abby", "fire");
+    $('#enemyNameDisplay').text(enemyProkemon.name);
+    $('#enemyTypeDisplay').text(enemyProkemon.type);
+    $('#enemyLevelDisplay').text(enemyProkemon.level);
+    $('#enemyHPDisplay').text(enemyProkemon.hp);
+    $('#enemyAbilitiesDisplay').text(enemyProkemon.ability);
+    $('#chooseEnemy').toggle();
+    $('#enemy').show();
+    $('#enemyFireImage').toggle();
+    //$('enemy-proke-stats').toggle();
   });
 });
+
+
+//$("body").css("background", "url('')");
